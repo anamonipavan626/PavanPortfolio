f8600bd9958a08e36ee73cf866b309959d501e74
anamonipavan626 <anamonipavan626@gmail.com>
2025-03-17 22:59:16 +0530

    Add project files.

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PavanPortfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult PortFolio()
+        {
+            return View();
+        }
+    }
+}
